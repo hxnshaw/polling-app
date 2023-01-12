96cd1bd65a0d11b33a6e4d65f1c988a7262e94d8
hxnshaw <eghosasere07@gmail.com>
2023-01-12 04:06:51 +0100

    routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const user = require("./routes/user");
+
+app.use(express.json());
+
+//ROUTERS
+app.use("/user", user);
+
 const port = 1000;
 
 app.listen(port, () => {
