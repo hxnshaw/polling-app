00281b726ab14794a3e793dcd4ad93203c4c2d09
hxnshaw <eghosasere07@gmail.com>
2023-01-18 11:56:18 +0100

    poll v1

---
+++ src/user-poll.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users(
+    id SERIAL NOT NULL PRIMARY KEY,
+    username VARCHAR(40) NOT NULL UNIQUE, 
+    email VARCHAR(40) NOT NULL UNIQUE,
+    passcode VARCHAR(255) NOT NULL,
+    poll_id BIGINT REFERENCES poll(id),
+
+)
+
+
+CREATE TABLE poll(
+    id SERIAL NOT NULL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    descriptions VARCHAR(255) NOT NULL,
+     
+)
+
+CREATE TABLE poll_answers(
+    id SERIAL NOT NULL PRIMARY KEY,
+    poll_id BIGINT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    votes VARCHAR(255) NOT NULL DEFAULT '0',
+)
